0645352e3c03ba9a0c9a50d9b62e44d1bdad3e8f
Zepeng Zhao <zxz174@student.bham.ac.uk>
2024-05-09 20:14:44 +0100

    reconstruct

--- php/form.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $value = $_GET["value"];
-    $_SESSION["value"] = $value;
-    echo "Value: " . $value;
-    echo "<br>";
-    echo '$_SESSION[\'value\']' . $_SESSION['value'];
-?>
